476f226e76f7bdbfa04385df01b7adadbcbd9916
DePram <dprameshwara@gmail.com>
2025-11-14 14:38:13 +0800

    revisi 1 done

--- script/main.js
+++ script/main.js
@@ -1,10 +1,13 @@
 const navbar = document.querySelector('#navbar');
-const style = ['backdrop-blur-md', 'bg-white/30', 'border-b', 'border-white/20'];
+const goUpBtn = document.querySelector(".goUp");
+const navbarStyle = ["backdrop-blur-md", "bg-white/30", "border-b", "border-white/20"];
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
-    navbar.classList.add(...style); 
+    navbar.classList.add(...navbarStyle);
+    goUpBtn.classList.remove("hidden");
   } else {
-    navbar.classList.remove(...style); 
+    navbar.classList.remove(...navbarStyle);
+    goUpBtn.classList.add("hidden");
   }
 });
