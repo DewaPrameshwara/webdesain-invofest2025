a11ddf223ce03bf3c06cc04b7e4ba681f6088613
DePram <dprameshwara@gmail.com>
2025-11-16 00:09:27 +0800

    perbaiki padding ledft di semua page, tools done sampe navigation, perbaiki isi tools navbar, bmi calculator ui done

---
+++ script/bmiCalculator.js
@@ -0,0 +1,7 @@
+const result = document.getElementById('result')
+const beratBadanValue = document.getElementById('beratBadan').value
+const tinggiBadanValue = document.getElementById('tinggiBadan').value
+const genderMale = document.getElementById('gender-male')
+const genderFemale = document.getElementById('gender-female')
+
+// alert(beratBadanValue, tinggiBadanValue)
